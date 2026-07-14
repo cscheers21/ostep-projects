500d7b7f6afd73c6cd53d624e027d35b5892a4e2
c_scheers <cscheers@ualberta.ca>
2026-07-13 19:06:10 -0600

    change exit(1) to return 1

--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -4,11 +4,11 @@ int main(int argc, char *argv[]) {
     if (argc < 2) {
         return 0;
     }
-    
+
     FILE *fp = fopen(argv[1], "r");
     if (fp == NULL) {
         printf("cannot open file\n");
-        exit(1);
+        return 1;
     }
 
     char buffer[100];
