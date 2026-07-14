e2b54e6e2f9e8d2a84dcc43343aaa84a6cea317a
c_scheers <cscheers@ualberta.ca>
2026-07-14 17:47:18 -0600

    finsih initial versoin

--- initial-utilities/wgrep/wgrep.c
+++ initial-utilities/wgrep/wgrep.c
@@ -1,4 +1,5 @@
 #include <stdio.h>;
+#include <string.h>;
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
@@ -12,11 +13,15 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    char *string = argv[2];
     char *line = NULL;
     size_t size = 0;
 
     while(getline(&line, &size, fp) != -1) {
-        
+            if (strstr(line, string) != NULL) {
+                printf("%s\n", line);
+            }
     }
+    
 
 }
