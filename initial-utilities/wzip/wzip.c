d67b7e10a1e097700342162fc014be1129a78173
c_scheers <cscheers@ualberta.ca>
2026-07-14 21:32:57 -0600

    first build wzip

--- initial-utilities/wzip/wzip.c
+++ initial-utilities/wzip/wzip.c
@@ -1,5 +1,50 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("wzip: file1 [file2 ...]\n");
+        return 1;
+    }
+
+    FILE *fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        printf("Could not open file\n");
+        return 1;
+    }
+
+    char prev_char = fgetc(fp);
+    if (prev_char == EOF) {
+        return 0;
+    }
+
+    int counter = 1;
+
+    char sent_char = fgetc(fp);
+    if (sent_char == EOF) {
+        fwrite(&counter, sizeof(int), 1, stdout);
+        fwrite(&prev_char, sizeof(char), 1, stdout);
+        return 0;
+    }
+
+    while (sent_char != EOF) {
+        if (sent_char != prev_char) {
+            fwrite(&counter, sizeof(int), 1, stdout);
+            fwrite(&prev_char, sizeof(char), 1, stdout);
+            counter = 1;
+        }
+        else {
+            counter++;
+        }
+
+        prev_char = sent_char;
+        sent_char = fgetc(fp);
+        
+        return 0;
+    }
+
+
     
+
+
+
 }
