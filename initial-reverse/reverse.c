004dd318a535d286b2978b47cad24b44690b0ee4
c_scheers <cscheers@ualberta.ca>
2026-08-11 19:25:05 -0600

    implemented fancyString for 1 file input

--- initial-reverse/reverse.c
+++ initial-reverse/reverse.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "fancystring.h"
 
-int main(int argc, char argv[]) {
+int main(int argc, char *argv[]) {
     if (argc == 2){
         FILE *fp = fopen(argv[1], "r");
         if (fp == NULL) {
@@ -8,6 +10,19 @@ int main(int argc, char argv[]) {
             exit(1);
         }
 
+        //returns a linked list of lines from file, starting from last line
+        FancyString *line = fancy_readlines(fp);
+        FancyString *previousLine;
         
+        while (line != NULL) {
+            printf("%s", line->string);
+
+            previousLine = line;
+            line = previousLine->prev;
+
+            FancyString_free(previousLine);
+        }
+        fclose(fp);
+        return 0;
     }
 }
